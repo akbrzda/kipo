727988853ae2b390e6862af06d466653f188c830
Akmal Akbarzoda <iamtilloev@gmail.com>
2022-05-05 03:14:05 +0700

    Added header

---
+++ dist/js/main.js
@@ -0,0 +1,1 @@
+!function(){var __webpack_modules__={"./dist/js/script.js":function(){eval("\n\n//# sourceURL=webpack://online-course/./dist/js/script.js?")}},__webpack_exports__={};__webpack_modules__["./dist/js/script.js"]()}();
