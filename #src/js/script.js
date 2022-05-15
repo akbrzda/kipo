8020b4d2aecb58aad4530a0459858631beb6e7c2
Akmal Akbarzoda <iamtilloev@gmail.com>
2022-05-15 13:19:05 +0700

    This is commit? o_O

--- #src/js/script.js
+++ #src/js/script.js
@@ -14,6 +14,21 @@ if (setTimeout(10), $(window).width() > "968") {
 	return $(this).parent().hasClass("hover") ? ($(this).parent().removeClass("hover topbar__menu-item--active"), $(this).parent().find(".topbar__dropdown-box").css("min-height", "0")) : ($(".hover .topbar__dropdown-box").css("min-height", "0"), $(".hover").removeClass("hover topbar__menu-item--active"), $(this).parent().addClass("hover topbar__menu-item--active"), $(this).parent().find(".topbar__dropdown-box").css("min-height", $(this).parent().find(".topbar__dropdown-box").data("height"))), !1
 }));
 
+if (setTimeout(10), $(window).width() > "968") {
+	var i = setTimeout((function () {}), 0);
+	$(".has-dropdown > a, .submenu__box").hover((function () {
+		$(".hover").removeClass("hover menu__bottom-item--active"), $(".submenu__box").css("min-height", "0"), $(this).parent().addClass("hover menu__bottom-item--active"), $(this).parent().find(".submenu__box").css("min-height", $(this).parent().find(".submenu__box").data("height")), clearTimeout(i)
+	}), (function () {
+		var e = $(this);
+		clearTimeout(i), i = setTimeout((function () {
+			$(".hover").removeClass("hover menu__bottom-item--active"), e.parent().find(".submenu__box").css("min-height", "0")
+		}), 200)
+	})), $(".submenu__box").click((function () {
+		clearTimeout(i)
+	}))
+} else $(".has-dropdown > a").click((function () {
+	return $(this).parent().hasClass("hover") ? ($(this).parent().removeClass("hover menu__bottom-item--active"), $(this).parent().find(".submenu__box").css("min-height", "0")) : ($(".hover .submenu__box").css("min-height", "0"), $(".hover").removeClass("hover menu__bottom-item--active"), $(this).parent().addClass("hover menu__bottom-item--active"), $(this).parent().find(".submenu__box").css("min-height", $(this).parent().find(".submenu__box").data("height"))), !1
+}));
 
 $(".stages__list-item").on("click", function (e) {
 	e.preventDefault;
@@ -57,10 +72,10 @@ $('.partners__list').slick({
 // });
 var swiper = new Swiper(".slider__list", {
 	loop: true,
-	       autoplay: {
-	       	delay: 2500,
-	       	disableOnInteraction: false,
-	       },
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
 	navigation: {
 		nextEl: ".slider-next",
 		prevEl: ".slider-prev",
@@ -76,4 +91,13 @@ function mobileMenu() {
 	burger.classList.toggle("active");
 	menu.classList.toggle("active");
 	body.classList.toggle("hidden");
+}
+
+const searchBtn = document.querySelector(".header__search-btn");
+const searchBox = document.querySelector(".header__search")
+searchBtn.addEventListener("click", headerSearch);
+
+function headerSearch() {
+	searchBtn.classList.toggle("active");
+	searchBox.classList.toggle("active");
 }
